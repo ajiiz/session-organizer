7f445252670d782f4766976509abd4d16c5a4e3a
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-02-18 15:04:23 +0100

    Added input component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
 import BlueBackground from "assets/blue-background.jpg";
+import Input from "styled/components/shared/Input";
+import { HeaderText } from "styled/elements/authentication-page/HeaderText";
 
 const Login: NextPage = () => {
   return (
     <div style={{ width: "100%", height: "100vh", display: "flex" }}>
-      <div style={{ width: "40%", height: "100%" }}></div>
+      <div style={{ width: "40%", height: "100%", display: "flex", justifyContent: "center" }}>
+        <div style={{ marginTop: "20vh" }}>
+          <HeaderText>Sign in to your account</HeaderText>
+          <Input text="Email adress" type="text" placeholder="E-mail" value="AAAA" name="email" />
+          <Input text="Password" type="password" placeholder="password" value="AAAA" name="password" />
+        </div>
+      </div>
       <div style={{ display: "flex", width: "60%", height: "100%" }}>
         <img style={{ width: "100%", height: "100%" }} src={BlueBackground.src} alt="blue-background" />
       </div>
