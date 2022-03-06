456a614419f652bac49d6cb136b0ded1991b1fa6
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-03-06 01:02:38 +0100

    Modified schema and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { hash } from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 const load = async (): Promise<void> => {
+  const hashedPassword = await hash("password", 12);
   await prisma.user.create({
     data: {
       firstName: "Piotr",
       lastName: "Wrobel",
-      login: "ajiiz",
-      password: "password",
+      email: "ajiiz@gmail.com",
+      phoneNumber: "000111222",
+      password: hashedPassword,
       role: "admin"
     }
   });
-  console.log("Added default users");
+  console.log("Added default user");
 };
 
 load()
