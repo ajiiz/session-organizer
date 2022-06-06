2090d43dd45c1be7bfea3ddc9405f7bb690289d5
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-06-06 17:09:38 +0200

    Added new colors

--- src/styled/base/Colors.tsx
+++ src/styled/base/Colors.tsx
@@ -1,5 +1,15 @@
 import { Property } from "csstype";
 
-export const BlackColor: Property.Color = "#000000";
-export const WhiteColor: Property.Color = "#FFFFFF";
-export const LightGrayColor: Property.Color = "#CCD2E3";
+export const WhiteColor: Property.Color = "#FFFEFC";
+export const DarkGrayColor: Property.Color = "#9B9A97";
+export const LightGrayColor: Property.Color = "#666666";
+export const GrayColor: Property.Color = "#37352F";
+export const BlackColor: Property.Color = "#111111";
+export const PinkColor: Property.Color = "#AD1A72";
+export const RedColor: Property.Color = "#E03E3E";
+export const OrangeColor: Property.Color = "#D9730D";
+export const YellowColor: Property.Color = "#DFAB01";
+export const GreenColor: Property.Color = "#0F7B6C";
+export const PurpleColor: Property.Color = "#6940A5";
+export const BlueColor: Property.Color = "#0B6E99";
+export const BrownColor: Property.Color = "#64473A";
