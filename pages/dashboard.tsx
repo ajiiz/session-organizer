351da2e6370334e4666d6640fb0db39083d4ba2b
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-06-06 16:53:55 +0200

    Initialized dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,32 @@
+import type { GetServerSideProps, NextPage } from "next";
+import { getSession, signOut, useSession } from "next-auth/react";
+
+const Dashboard: NextPage = () => {
+  const session = useSession();
+
+  return (
+    <div>
+      <div>Dashboard available!</div>
+      {session.data?.user && <button onClick={() => signOut()}>Sign out</button>}
+    </div>
+  );
+};
+
+export default Dashboard;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/"
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+};
