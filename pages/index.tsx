93ed70e94c6f8c95c698493bf7245356e01c516b
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-06-06 16:54:18 +0200

    Initialized landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { GetServerSideProps, NextPage } from "next";
-import { getSession, signOut, useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  const session = useSession();
-
   return (
     <div>
-      <div>Hello session exam!</div>
-      {session.data?.user && <button onClick={() => signOut()}>Sign out</button>}
+      <div>Landing page</div>
     </div>
   );
 };
@@ -16,6 +13,15 @@ export default Home;
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
+
+  if (session?.user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/dashboard"
+      }
+    };
+  }
 
   if (!session?.user) {
     return {
