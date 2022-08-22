5e50d175b1b1d18be110865ec3bb7e0c424281da
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-08-22 16:31:41 +0200

    Modified imports

--- src/styled/components/landing/index.tsx
+++ src/styled/components/landing/index.tsx
@@ -1,9 +1,9 @@
-import { Wrapper } from "styled/elements/shared/Wrapper";
-import Navbar from "./Navbar";
+import { Wrapper } from "styled/elements/shared/wrappers/Wrapper";
+import Navbar from "../navbar/Navbar";
 
 const Landing = () => {
   return (
-    <Wrapper flexDirection="column" height="100vh" alignItems={"flex-start"} justifyContent="flex-start">
+    <Wrapper flexDirection="column" height="100vh" alignItems="flex-start" justifyContent="flex-start">
       <Navbar />
     </Wrapper>
   );
