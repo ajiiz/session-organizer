e7430ae1d2177bb2bc138b1e44a0f903bcc30d6f
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-08-25 16:52:33 +0200

    Added navbar props

--- src/styled/components/navbar/Navbar.tsx
+++ src/styled/components/navbar/Navbar.tsx
@@ -5,29 +5,35 @@ import * as S from "./navbar.styled";
 import Button from "../shared/button/Button";
 import { Colors } from "styled/base/Colors";
 
-const Navbar = () => {
+interface Props {
+  removeButtons?: boolean;
+}
+
+const Navbar = ({ removeButtons }: Props) => {
   return (
     <S.Wrapper>
       <S.LogoWrapper>
         <StyledLogo src={Logo} />
         <S.LogoContent>Listic</S.LogoContent>
       </S.LogoWrapper>
-      <S.ButtonsWrapper>
-        <Button backgroundColor="" backgroundOpacity="1" hoverColor={Colors.GrayColor} hoverOpacity="0.1">
-          Log in
-        </Button>
-        <S.RegisterButtonWrapper>
-          <Button
-            fontColor={Colors.WhiteColor}
-            backgroundColor={Colors.GreenColor}
-            backgroundOpacity="0.8"
-            hoverColor={Colors.GreenColor}
-            hoverOpacity="1"
-          >
-            Register now
+      {!removeButtons && (
+        <S.ButtonsWrapper>
+          <Button backgroundColor="" backgroundOpacity="1" hoverColor={Colors.GrayColor} hoverOpacity="0.1">
+            Log in
           </Button>
-        </S.RegisterButtonWrapper>
-      </S.ButtonsWrapper>
+          <S.RegisterButtonWrapper>
+            <Button
+              fontColor={Colors.WhiteColor}
+              backgroundColor={Colors.GreenColor}
+              backgroundOpacity="0.8"
+              hoverColor={Colors.GreenColor}
+              hoverOpacity="1"
+            >
+              Register now
+            </Button>
+          </S.RegisterButtonWrapper>
+        </S.ButtonsWrapper>
+      )}
     </S.Wrapper>
   );
 };
