bb7fe120ad34413423d09b7ed93ff16938bf4386
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-08-30 16:49:18 +0200

    Added landing components

---
+++ src/styled/components/landing/Landing.styled.ts
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import Image from "next/image";
+import { Colors } from "styled/base/Colors";
+import { device } from "styled/base/Responsive";
+import { Property } from "csstype";
+
+export const HeroWrapper = styled.div`
+  height: 88%;
+  width: 100%;
+  padding: 0 15% 10% 15%;
+  display: flex;
+  align-items: center;
+
+  ${device.smalldesktop} {
+    height: fit-content;
+    padding: 10%;
+  }
+
+  ${device.tablet} {
+    flex-direction: column-reverse;
+  }
+`;
+
+export const HeroContent = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  ${device.tablet} {
+    width: 100%;
+    margin-top: 4rem;
+    align-items: center;
+  }
+
+  ${device.mobile} {
+    margin: 0;
+  }
+`;
+
+export const HeaderWrapper = styled.div`
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 4.75em;
+
+  ${device.smalldesktop} {
+    font-size: 3.75em;
+  }
+
+  ${device.tablet} {
+    text-align: center;
+  }
+
+  ${device.mobile} {
+    font-size: 3.6em;
+  }
+`;
+
+export const Paragraph = styled.p`
+  width: 80%;
+  margin: 0 0 2rem 0;
+  padding: 0;
+  font-size: 1.3em;
+  line-height: 150%;
+  color: ${Colors.LightGrayColor};
+
+  ${device.smalldesktop} {
+    width: 100%;
+    font-size: 1.2em;
+  }
+
+  ${device.tablet} {
+    text-align: center;
+  }
+
+  ${device.mobile} {
+    font-size: 1.1em;
+  }
+`;
+
+interface ImageWrapperProps {
+  width?: Property.Width;
+  height?: Property.Height;
+}
+
+export const ImageWrapper = styled.div<ImageWrapperProps>`
+  width: ${props => (props.width ? props.width : "100%")};
+  height: ${props => (props.height ? props.height : "100%")};
+  display: flex;
+
+  ${device.smalldesktop} {
+    width: 50%;
+  }
+
+  ${device.tablet} {
+    width: 60%;
+  }
+
+  ${device.mobile} {
+    display: none;
+  }
+`;
+
+export const HeroImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  align-self: center;
+`;
---
+++ src/styled/components/landing/Landing.tsx
@@ -0,0 +1,40 @@
+import * as S from "./Landing.styled";
+import { Wrapper } from "styled/elements/shared/wrappers/Wrapper";
+import Navbar from "styled/components/navbar/Navbar";
+import Button from "styled/components/shared/button/Button";
+import { Colors } from "styled/base/Colors";
+import HeroImage from "assets/Hero-Image.svg";
+
+const Landing = () => {
+  return (
+    <Wrapper flexDirection="column" height="100vh" alignItems="flex-start" justifyContent="flex-start">
+      <Navbar />
+      <S.HeroWrapper>
+        <S.HeroContent>
+          <S.HeaderWrapper>
+            <S.Header>One workspace.</S.Header>
+            <S.Header>For every exam.</S.Header>
+          </S.HeaderWrapper>
+          <S.Paragraph>
+            More than a callendar or a paper. Process every important exam in one provided by Listic place.
+          </S.Paragraph>
+          <Button
+            fontColor={Colors.WhiteColor}
+            backgroundColor={Colors.GreenColor}
+            backgroundOpacity="0.8"
+            hoverColor={Colors.GreenColor}
+            hoverOpacity="1"
+            padding="0.65rem 3rem"
+          >
+            Register now
+          </Button>
+        </S.HeroContent>
+        <S.ImageWrapper width="50%" height="75%">
+          <S.HeroImage src={HeroImage} alt="Hero" placeholder="blur" blurDataURL={HeroImage} />
+        </S.ImageWrapper>
+      </S.HeroWrapper>
+    </Wrapper>
+  );
+};
+
+export default Landing;
