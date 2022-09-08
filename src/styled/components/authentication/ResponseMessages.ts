903651ec83ab68131432b57b0f24bffca9bd3578
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-08 17:10:29 +0200

    Added responses enum

---
+++ src/styled/components/authentication/ResponseMessages.ts
@@ -0,0 +1,6 @@
+export enum ResponseMessages {
+  Null = "",
+  Invalid = "Your provided email is invalid. Provide correct email or sign up.",
+  Valid = "Your provided email is valid. Insert your password and sign in.",
+  Error = "The error has occured when trying to sign in. Please try again."
+}
