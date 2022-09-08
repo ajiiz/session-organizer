7f665d5acaf85f224b703cdad0d0f5bd8620c0ca
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-08 18:44:58 +0200

    Added register response messages

--- src/styled/components/authentication/ResponseMessages.ts
+++ src/styled/components/authentication/ResponseMessages.ts
@@ -1,6 +1,11 @@
-export enum ResponseMessages {
+export enum LoginResponseMessages {
   Null = "",
   Invalid = "Your provided email is invalid. Provide correct email or sign up.",
   Valid = "Your provided email is valid. Insert your password and sign in.",
   Error = "The error has occured when trying to sign in. Please try again."
+}
+
+export enum RegisterResponseMessages {
+  Invalid = "Your provided email is invalid. Please insert correct email.",
+  Error = "The error has occured when trying to sign up. Please try again."
 }
