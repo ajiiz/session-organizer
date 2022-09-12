a94fb7825da916d61126aa0fa1c38c76adabf04e
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-12 18:34:46 +0200

    Added wrapper in dashboard

--- src/styled/components/dashboard/DashboardComponent.tsx
+++ src/styled/components/dashboard/DashboardComponent.tsx
@@ -1,16 +1,17 @@
 import { signOut, useSession } from "next-auth/react";
 import NavigationPanel from "styled/components/navigation-panel/NavigationPanel";
+import { Wrapper } from "styled/elements/shared/wrappers/Wrapper";
 import * as S from "./Dashboard.styled";
 
 const DashboardComponent = () => {
   const session = useSession();
 
   return (
-    <>
+    <Wrapper flexDirection="row" height="100vh" alignItems="flex-start" justifyContent="flex-start">
       <NavigationPanel />
       <S.Section>Dashboard available!</S.Section>
       {session.data?.user && <button onClick={() => signOut({ callbackUrl: "/signin" })}>Sign out</button>}
-    </>
+    </Wrapper>
   );
 };
 
