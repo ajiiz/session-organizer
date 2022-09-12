5e21f45e0654de1bb3c22712cfe9b2796c9e43eb
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-12 18:34:29 +0200

    Initialized styled components

--- src/styled/components/navigation-panel/NavigationPanel.tsx
+++ src/styled/components/navigation-panel/NavigationPanel.tsx
@@ -1,10 +1,36 @@
+import Logo from "assets/Logo.svg";
+import { StyledLogo } from "styled/elements/shared/logo/Logo";
+import { goToLink } from "utils/NavigationUtilities";
 import * as S from "./NavigationPanel.styled";
 
 const NavigationPanel = () => {
   return (
-    <S.Section>
-      <p>Navigation Panel</p>
-    </S.Section>
+    <S.SectionWrapper>
+      <S.ContentWrapper>
+        <S.LogoContainer>
+          <S.LogoWrapper onClick={() => goToLink({ link: "/" })}>
+            <StyledLogo src={Logo} alt="Logo" />
+            <S.LogoContent>Listic</S.LogoContent>
+          </S.LogoWrapper>
+        </S.LogoContainer>
+        <S.CreationButtonWrapper>
+          <S.LinkIcon />
+          <S.LinkParagraph></S.LinkParagraph>
+        </S.CreationButtonWrapper>
+        <S.LinksWrapper>
+          <S.Link>
+            <S.LinkIcon />
+            <S.LinkParagraph></S.LinkParagraph>
+          </S.Link>
+        </S.LinksWrapper>
+      </S.ContentWrapper>
+      <S.AuthorizationWrapper>
+        <S.UserInformation>
+          Loggd in as <S.UserEmail>email@gmail.com</S.UserEmail>
+        </S.UserInformation>
+        <S.LogoutButton>Logout</S.LogoutButton>
+      </S.AuthorizationWrapper>
+    </S.SectionWrapper>
   );
 };
 
