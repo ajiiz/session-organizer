9494a15b93dffb940a9433a974295fb132e2f9ee
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-12 17:47:54 +0200

    Initialized navigation panel

---
+++ src/styled/components/navigation-panel/NavigationPanel.tsx
@@ -0,0 +1,11 @@
+import * as S from "./NavigationPanel.styled";
+
+const NavigationPanel = () => {
+  return (
+    <S.Section>
+      <p>Navigation Panel</p>
+    </S.Section>
+  );
+};
+
+export default NavigationPanel;
