a70abf4f71bce3f26513ee0949caf4b6bd98638a
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-12 17:48:01 +0200

    Imported navigation panel

--- src/styled/components/dashboard/DashboardComponent.tsx
+++ src/styled/components/dashboard/DashboardComponent.tsx
@@ -1,4 +1,5 @@
 import { signOut, useSession } from "next-auth/react";
+import NavigationPanel from "styled/components/navigation-panel/NavigationPanel";
 import * as S from "./Dashboard.styled";
 
 const DashboardComponent = () => {
@@ -6,6 +7,7 @@ const DashboardComponent = () => {
 
   return (
     <>
+      <NavigationPanel />
       <S.Section>Dashboard available!</S.Section>
       {session.data?.user && <button onClick={() => signOut({ callbackUrl: "/signin" })}>Sign out</button>}
     </>
