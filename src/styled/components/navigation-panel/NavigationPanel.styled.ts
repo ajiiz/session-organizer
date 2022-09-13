ee95ca4755aba8a060906ce2fd2270898a6730b0
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-13 16:26:24 +0200

    Added navigation bar links and footer styles

--- src/styled/components/navigation-panel/NavigationPanel.styled.ts
+++ src/styled/components/navigation-panel/NavigationPanel.styled.ts
@@ -4,17 +4,19 @@ import { Colors } from "styled/base/Colors";
 import Image from "next/image";
 
 export const SectionWrapper = styled.section`
-  width: 20vw;
+  width: 20rem;
   height: 100%;
   display: flex;
   flex-direction: column;
   background-color: ${hexToRgba(Colors.DarkWhiteColor, 0.5)};
   border-right: 0.5px solid ${hexToRgba(Colors.DarkGrayColor, 0.5)};
+  color: ${Colors.BlackColor};
 `;
 
 export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  height: 85%;
 `;
 
 export const LogoContainer = styled.div`
@@ -42,31 +44,88 @@ export const LogoContent = styled.h3`
 
 export const CreationButtonWrapper = styled.div`
   width: 100%;
-  padding: 1rem 0;
-  margin-left: 0.5rem;
+  padding: 2rem 2rem;
   display: flex;
 `;
 
-export const LinksWrapper = styled.div``;
-
-export const Link = styled.div`
-  width: 75%;
+export const LinksWrapper = styled.div`
+  width: 100%;
   display: flex;
+  flex-direction: column;
+  padding: 0 2rem;
+`;
+
+interface LinkProps {
+  margin?: string;
+}
+
+export const Link = styled.div<LinkProps>`
+  width: 100%;
+  padding: 0.2rem 1rem;
+  margin: ${props => props.margin ?? ""};
+  display: flex;
+  align-items: center;
   cursor: pointer;
+  border-radius: 4px;
+  transition: 0.2s background linear;
 
   &:hover {
-    background-color: black;
+    background: ${hexToRgba(Colors.DarkGrayColor, 0.15)};
   }
 `;
 
-export const LinkParagraph = styled.span``;
+export const LinkParagraph = styled.span`
+  margin-left: 1.1rem;
+  font-family: InterRegular;
+  font-size: 0.9em;
+`;
 
 export const LinkIcon = styled(Image)``;
 
-export const AuthorizationWrapper = styled.div``;
+export const AuthorizationWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-export const UserInformation = styled.p``;
+export const UserInformation = styled.p`
+  text-align: center;
+  font-family: InterLight;
+  font-size: 0.8em;
+`;
 
-export const UserEmail = styled.span``;
+export const UserEmail = styled.span`
+  font-family: InterRegular;
+  color: ${Colors.GreenColor};
+  text-decoration: underline;
+  text-decoration-color: ${Colors.GreenColor};
+  letter-spacing: 0.05em;
+`;
 
-export const LogoutButton = styled.button``;
+export const LogoutButton = styled.button`
+  width: 90%;
+  margin: 0 auto;
+  padding: 0.4rem 1.5rem;
+  text-align: center;
+  border: 1px solid ${Colors.RedColor};
+  border-radius: 4px;
+  background: ${hexToRgba(Colors.RedColor, 0.25)};
+  font-size: 0.9em;
+  color: ${Colors.RedColor};
+  transition: 0.2s background linear;
+
+  &:hover {
+    padding: 0.4rem 1.5rem;
+    font-size: 0.9em;
+    border: 1px solid ${Colors.RedColor};
+    background: ${hexToRgba(Colors.RedColor, 0.3)};
+    color: ${Colors.RedColor};
+  }
+
+  &:focus {
+    padding: 0.4rem 1.5rem;
+    font-size: 0.9em;
+    border: 1px solid ${Colors.RedColor};
+    background: ${hexToRgba(Colors.RedColor, 0.25)};
+    color: ${Colors.RedColor};
+  }
+`;
