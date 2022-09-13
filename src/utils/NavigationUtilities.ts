93ce2dc6011089d8ffb5c4d817708b14dafc1431
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-13 18:12:17 +0200

    Added isCurrentPath method

--- src/utils/NavigationUtilities.ts
+++ src/utils/NavigationUtilities.ts
@@ -7,3 +7,12 @@ interface LinkProp {
 export const goToLink = ({ link }: LinkProp): void => {
   router.push(link);
 };
+
+interface CurrentPathProps {
+  routerPath: string;
+  currentPath: string;
+}
+
+export const isCurrentPath = ({ routerPath, currentPath }: CurrentPathProps): boolean => {
+  return routerPath === currentPath;
+};
