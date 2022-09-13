8dd31d4ea1daa76febdd61276d579918cc35b561
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-13 18:02:07 +0200

    Added panels logic

--- src/styled/components/dashboard/Dashboard.styled.ts
+++ src/styled/components/dashboard/Dashboard.styled.ts
@@ -1,3 +1,10 @@
 import styled from "styled-components";
+import { device } from "styled/base/Responsive";
 
-export const Section = styled.div``;
+export const Section = styled.div`
+  width: 80%;
+
+  ${device.smalldesktop} {
+    width: 100%;
+  }
+`;
--- src/styled/components/dashboard/DashboardComponent.tsx
+++ src/styled/components/dashboard/DashboardComponent.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import NavigationPanel from "styled/components/navigation-panel/NavigationPanel";
 import { Wrapper } from "styled/elements/shared/wrappers/Wrapper";
+import DashboardNavbar from "./dashboard-navbar/DashboardNavbar";
 import * as S from "./Dashboard.styled";
 
 const DashboardComponent = () => {
+  const [isControlPanel, setIsControlPanel] = useState(false);
+  const [isCallendar, setIsCallendar] = useState(false);
+
+  const handleNavigationPanel = (state?: boolean) => {
+    if (state) {
+      setIsControlPanel(state);
+    } else {
+      setIsControlPanel(!isControlPanel);
+    }
+
+    setIsCallendar(false);
+  };
+  const handleCallendarPanel = (state?: boolean) => {
+    if (state) {
+      setIsCallendar(state);
+    } else {
+      setIsCallendar(!isCallendar);
+    }
+    setIsControlPanel(false);
+  };
+
   return (
     <Wrapper flexDirection="row" height="100vh" alignItems="flex-start" justifyContent="flex-start">
-      <NavigationPanel />
-      <S.Section>Dashboard available!</S.Section>
+      <NavigationPanel isOpen={isControlPanel} handleOpen={handleNavigationPanel} />
+      <S.Section>
+        <DashboardNavbar handleNavigationPanel={handleNavigationPanel} handleCallendarPanel={handleCallendarPanel} />
+        Dashboard available!
+      </S.Section>
     </Wrapper>
   );
 };
