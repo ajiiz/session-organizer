81dfd2719b2a6de676458ce90bcfad96c1f48af9
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-19 18:43:40 +0200

    Initialized event cards layout

--- src/styled/components/dashboard/dashboard-main/DashboardMain.tsx
+++ src/styled/components/dashboard/dashboard-main/DashboardMain.tsx
@@ -25,6 +25,30 @@ const DashboardMain = () => {
         <S.Header>Your todays upcoming events</S.Header>
         <S.Paragraph>View your future events and filter them by date.</S.Paragraph>
       </S.TextWrapper>
+      <S.EventsWrapper>
+        {events ? (
+          events.map(event => (
+            /* This should be another component */
+            <S.EventCard key={event.id}>
+              <S.EventHeader>{event.name}</S.EventHeader>
+              <S.EventParagraph>{event.description}</S.EventParagraph>
+              <S.EventDateTime>
+                <S.EventTime>
+                  <S.EventTimeName>Start time</S.EventTimeName>
+                  <S.EventTimeDetails>{event.startDate}</S.EventTimeDetails>
+                </S.EventTime>
+                <S.EventTime>
+                  <S.EventTimeName>End time</S.EventTimeName>
+                  <S.EventTimeDetails>{event.endDate}</S.EventTimeDetails>
+                </S.EventTime>
+              </S.EventDateTime>
+            </S.EventCard>
+          ))
+        ) : (
+          /* This should be another component */
+          <div>There will be component that shows not found</div>
+        )}
+      </S.EventsWrapper>
     </S.ContentWrapper>
   );
 };
