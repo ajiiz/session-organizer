0fbaa0276427658116f6d413e1e3897cf5bccf46
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-20 17:19:36 +0200

    Fixed conditions and added the button

--- src/styled/components/dashboard/dashboard-main/DashboardMain.tsx
+++ src/styled/components/dashboard/dashboard-main/DashboardMain.tsx
@@ -1,10 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Event } from "@prisma/client";
 import { getEvents } from "network/events/getEvents";
+import moment from "moment";
 import * as S from "./DashboardMain.styled";
+import { getTime } from "utils/DateUtilities";
 
 const DashboardMain = () => {
   const [events, setEvents] = useState<null | Event[]>();
+  const [shouldShowAll, setShouldShowAll] = useState(false);
+  const currentShowedEvents = useMemo(() => {
+    if (!shouldShowAll) {
+      return events?.slice(0, 3);
+    }
+    return events;
+  }, [events, shouldShowAll]);
 
   useEffect(() => {
     fetchEvents();
@@ -26,24 +35,29 @@ const DashboardMain = () => {
         <S.Paragraph>View your future events and filter them by date.</S.Paragraph>
       </S.TextWrapper>
       <S.EventsWrapper>
-        {events ? (
-          events.map(event => (
-            /* This should be another component */
-            <S.EventCard key={event.id}>
-              <S.EventHeader>{event.name}</S.EventHeader>
-              <S.EventParagraph>{event.description}</S.EventParagraph>
-              <S.EventDateTime>
-                <S.EventTime>
-                  <S.EventTimeName>Start time</S.EventTimeName>
-                  <S.EventTimeDetails>{event.startDate}</S.EventTimeDetails>
-                </S.EventTime>
-                <S.EventTime>
-                  <S.EventTimeName>End time</S.EventTimeName>
-                  <S.EventTimeDetails>{event.endDate}</S.EventTimeDetails>
-                </S.EventTime>
-              </S.EventDateTime>
-            </S.EventCard>
-          ))
+        {currentShowedEvents ? (
+          <>
+            {currentShowedEvents.map(event => (
+              /* This should be another component */
+              <S.EventCard key={event.id}>
+                <S.EventHeader>{event.name}</S.EventHeader>
+                <S.EventParagraph>{event.description}</S.EventParagraph>
+                <S.EventDateTime>
+                  <S.EventTime>
+                    <S.EventTimeName>Start time</S.EventTimeName>
+                    <S.EventTimeDetails>{getTime(event.startDate)}</S.EventTimeDetails>
+                  </S.EventTime>
+                  <S.EventTime>
+                    <S.EventTimeName>End time</S.EventTimeName>
+                    <S.EventTimeDetails>{getTime(event.endDate)}</S.EventTimeDetails>
+                  </S.EventTime>
+                </S.EventDateTime>
+              </S.EventCard>
+            ))}
+            <S.Button type="button" onClick={() => setShouldShowAll(!shouldShowAll)}>
+              {shouldShowAll ? "Show only first three" : "Show all"}
+            </S.Button>
+          </>
         ) : (
           /* This should be another component */
           <div>There will be component that shows not found</div>
