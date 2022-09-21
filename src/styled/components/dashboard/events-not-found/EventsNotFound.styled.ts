2a8d81f3ba34c6dec7b3c643cf6a2376a042b0da
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-09-21 17:29:39 +0200

    Added events not found component

--- src/styled/components/dashboard/events-not-found/EventsNotFound.styled.ts
+++ src/styled/components/dashboard/events-not-found/EventsNotFound.styled.ts
@@ -1,3 +1,49 @@
 import styled from "styled-components";
+import Image from "next/image";
+import { Colors } from "styled/base/Colors";
+import { Property } from "csstype";
+import { device } from "styled/base/Responsive";
 
-export const Wrapper = styled.div``;
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+
+  ${device.tablet} {
+    text-align: center;
+  }
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  font-family: InterSemiBold;
+  font-size: 2.8em;
+`;
+
+export const Paragraph = styled.p`
+  margin: 1.5rem 0 0 0.3rem;
+  font-size: 0.9em;
+  letter-spacing: 0.05em;
+  color: ${Colors.LightGrayColor};
+`;
+
+interface ImageWrapperProps {
+  width?: Property.Width;
+  height?: Property.Height;
+}
+
+export const ImageWrapper = styled.div<ImageWrapperProps>`
+  width: 100%;
+  height: 100%;
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+
+  ${device.tablet} {
+    margin-top: 6rem;
+  }
+`;
+
+export const NotFoundImage = styled(Image)`
+  align-self: center;
+`;
--- src/styled/components/dashboard/events-not-found/EventsNotFound.tsx
+++ src/styled/components/dashboard/events-not-found/EventsNotFound.tsx
@@ -1,10 +1,17 @@
+import NotFoundImage from "assets/Not-Found-Image.svg";
 import * as S from "./EventsNotFound.styled";
 
 const EventsNotFound = () => {
   return (
-    <div>
-      <p>There will be events not found component</p>
-    </div>
+    <>
+      <S.TextWrapper>
+        <S.Header>You don’t have any events</S.Header>
+        <S.Paragraph>Change the date to the check other days.</S.Paragraph>
+      </S.TextWrapper>
+      <S.ImageWrapper>
+        <S.NotFoundImage src={NotFoundImage} alt="Hero" placeholder="blur" blurDataURL={NotFoundImage} />
+      </S.ImageWrapper>
+    </>
   );
 };
 
