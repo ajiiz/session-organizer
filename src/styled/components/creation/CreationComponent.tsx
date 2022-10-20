cc95dea46e17977081caa8bdd8e591975f85cef5
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-10-20 15:23:13 +0200

    Initialized creation page component

---
+++ src/styled/components/creation/CreationComponent.tsx
@@ -0,0 +1,11 @@
+import PageTemplate from "styled/components/page-template/PageTemplate";
+
+const CreationComponent = () => {
+  return (
+    <PageTemplate>
+      <p>Creation Here</p>
+    </PageTemplate>
+  );
+};
+
+export default CreationComponent;
