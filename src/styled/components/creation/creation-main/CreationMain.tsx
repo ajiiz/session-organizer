afd97c3f04f753d05979b1f813bb3d352a87ff70
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-10-24 16:38:53 +0200

    Implemented method to get all the options

--- src/styled/components/creation/creation-main/CreationMain.tsx
+++ src/styled/components/creation/creation-main/CreationMain.tsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
+import { USER_ROLES } from "utils/Constants";
 import PageInformation from "styled/components/shared/page-info/PageInformation";
-import * as S from "./CreationMain.styled";
+import { useSession } from "next-auth/react";
+import { isGroupAdmin } from "network/users/isGroupAdmin";
 import * as SS from "styled/components/shared/page-wrapper/PageWrapper.styled";
 
 const CreationMain = () => {
+  const { data: session, status } = useSession();
+  const [selectedOption, setSelectedOption] = useState("custom");
+  const [options, setOptions] = useState<string[]>([]);
+
+  const getOptions = async () => {
+    const options = ["custom", "group", "group event", "request"];
+    const userRole = session?.user.role;
+    const userEmail = session?.user.email ?? "";
+    let groupAdminExist = false;
+
+    try {
+      const data = await isGroupAdmin({ userEmail: userEmail });
+      groupAdminExist = data.isGroupAdmin;
+    } catch (error) {
+      console.error(error);
+    }
+
+    if (userRole === USER_ROLES.administrator) return options;
+    if (userRole === USER_ROLES.examinator) return options.filter(option => option !== "request");
+    if (userRole === USER_ROLES.student && groupAdminExist)
+      return options.filter(option => option !== "group" && option !== "group event");
+    return options.filter(option => option === "custom");
+  };
+
+  const handleGetOptions = async () => {
+    const filteredOptions = await getOptions();
+    setOptions(filteredOptions);
+  };
+
+  useEffect(() => {
+    if (status !== "loading") {
+      handleGetOptions();
+    }
+  }, [status]);
+
   return (
     <SS.ContentWrapper>
       <SS.ContentContainer>
