582fca9acc61dd28052582a8532ce474868fa649
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-10-27 18:11:16 +0200

    Initialized simple render logic

--- src/styled/components/creation/creation-inputs/CreationInputs.tsx
+++ src/styled/components/creation/creation-inputs/CreationInputs.tsx
@@ -1,11 +1,18 @@
-import CustomInputs from "./inputs/CustomInputs";
+import CustomInputs from "styled/components/creation/creation-inputs/inputs/CustomInputs";
+import { FormData } from "styled/components/creation/useCreation";
 
 type Props = {
   selectedOption: string;
+  formData: FormData | null;
+  handleFormDataChange: (data: FormData) => void;
 };
 
-const CreationInputs = ({ selectedOption }: Props) => {
-  return <>{selectedOption === "custom" && <CustomInputs />}</>;
+const CreationInputs = ({ selectedOption, formData, handleFormDataChange }: Props) => {
+  return (
+    <>
+      {selectedOption === "custom" && <CustomInputs formData={formData} handleFormDataChange={handleFormDataChange} />}
+    </>
+  );
 };
 
 export default CreationInputs;
