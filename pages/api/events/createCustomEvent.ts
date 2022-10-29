0375808f5955be88ad40ac8e00f8cca4ff478393
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-10-29 12:29:20 +0200

    Added custom event creation endpoint

---
+++ pages/api/events/createCustomEvent.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiHandler } from "next";
+import { getSession } from "next-auth/react";
+import { CustomEventFormData } from "styled/components/creation/useCreation";
+
+export const path = "api/events/createCustomEvent";
+
+export const createCustomEvent: NextApiHandler<CustomEventFormData> = async (req, res) => {
+  const prisma = new PrismaClient();
+
+  const session = await getSession({ req });
+  if (!session?.user?.email) {
+    res.statusMessage = `User is not logged in`;
+    res.status(400).end();
+    return;
+  }
+
+  const user = await prisma.user.findFirst({
+    where: { email: session.user.email }
+  });
+  if (!user) {
+    res.statusMessage = `User could not be found`;
+    res.status(400).end();
+    return;
+  }
+
+  const { name, details, startDate, endDate, startTime, endTime } = req.body.params as CustomEventFormData;
+
+  if (!name || !details || !startDate || !endDate || !startTime || !endTime) {
+    console.log(req.body);
+    res.statusMessage = `Malformed request data`;
+    res.status(400).end();
+    return;
+  }
+
+  const startDateArray = startDate.split(".").map(Number);
+  const startTimeArray = startTime.split(":").map(Number);
+  const convertedStartDate = new Date(
+    startDateArray[2],
+    startDateArray[1] - 1,
+    startDateArray[0],
+    startTimeArray[0],
+    startTimeArray[1]
+  );
+  const endDateArray = endDate.split(".").map(Number);
+  const endTimeArray = endTime.split(":").map(Number);
+  const convertedEndDate = new Date(
+    endDateArray[2],
+    endDateArray[1] - 1,
+    endDateArray[0],
+    endTimeArray[0],
+    endTimeArray[1]
+  );
+
+  console.log(convertedStartDate, convertedEndDate);
+
+  const newEvent = await prisma.event.create({
+    data: {
+      name,
+      description: details,
+      startDate: convertedStartDate,
+      endDate: convertedEndDate,
+      userId: user.id,
+      status: "future"
+    }
+  });
+  if (!newEvent) {
+    res.statusMessage = `Event could not be created`;
+    res.status(400).end();
+    return;
+  }
+
+  prisma.$disconnect();
+
+  res.status(200).end();
+};
+
+export default createCustomEvent;
