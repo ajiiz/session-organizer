d5d9c67438fcd9859cc8637332a6dbf59f70853d
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-10-31 19:58:41 +0100

    Added utility method to get datetime from string

--- src/utils/DateUtilities.ts
+++ src/utils/DateUtilities.ts
@@ -3,3 +3,9 @@ import moment from "moment";
 export const getTime = (date: Date): string => {
   return moment(date).format("HH:mm");
 };
+
+export const getDateTimeFromString = (date: string, time: string): Date => {
+  const dateArray = date.split(".").map(Number);
+  const timeArray = time.split(":").map(Number);
+  return new Date(dateArray[2], dateArray[1] - 1, dateArray[0], timeArray[0], timeArray[1]);
+};
