0e9b3158953a8df3980e71a69211803d3f1b4042
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-11-04 22:42:33 +0100

    Added modal component

---
+++ src/styled/components/shared/popup/Modal.tsx
@@ -0,0 +1,34 @@
+import CheckIcon from "assets/icons/check-icon.svg";
+import { useEffect } from "react";
+import * as S from "./Modal.styled";
+
+export type ModalType = "success" | "error";
+
+type Props = {
+  isOpen: boolean;
+  handleModal: (value: boolean) => void;
+  text?: string;
+  type?: ModalType;
+};
+
+const Modal = ({ isOpen, handleModal, text = defaultModalText, type = "success" }: Props) => {
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const timeout = setTimeout(() => handleModal(false), 3500);
+    return () => clearTimeout(timeout);
+  }, [isOpen]);
+
+  return (
+    <S.ModalWrapper isOpen={isOpen} onClick={() => handleModal(false)} type={type}>
+      <S.ModalContainer>
+        <S.ModalText>{text}</S.ModalText>
+        <S.CheckIcon src={CheckIcon} alt="check" />
+      </S.ModalContainer>
+    </S.ModalWrapper>
+  );
+};
+
+export default Modal;
+
+const defaultModalText = "Your action was successful!";
