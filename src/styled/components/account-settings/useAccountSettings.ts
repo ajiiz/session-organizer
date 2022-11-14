e8d68acad99bf6d8c79b9c55dae9ff181639c798
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-11-14 15:34:56 +0100

    Initialized account settings hook

---
+++ src/styled/components/account-settings/useAccountSettings.ts
@@ -0,0 +1,115 @@
+import { useEffect, useMemo, useState } from "react";
+
+export interface useAccountSettingsProps {
+  selectedOption: string;
+  options: string[];
+  handleOptionChange: (option: string) => void;
+  handleModal: (value: boolean) => void;
+  formData: FormData;
+  handleFormDataChange: (data: FormData) => void;
+  handleAccountSave: () => void;
+  isFormValid: boolean;
+  isModalOpen: boolean;
+  isAccount: boolean;
+}
+
+export type AccountFormData = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  number: string;
+};
+
+export type FormData = AccountFormData | null;
+
+const DEFAULT_OPTIONS = ["account", "group"];
+const DEFAULT_ACCOUNT_FORM_DATA = { email: "", firstName: "", lastName: "", password: "", number: "" };
+
+export const useAccountSettings = (): useAccountSettingsProps => {
+  const [selectedOption, setSelectedOption] = useState<string>("account");
+  const [options, setOptions] = useState<string[]>([]);
+  const [formData, setFormData] = useState<FormData>(DEFAULT_ACCOUNT_FORM_DATA);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isAccount = useMemo(() => selectedOption === "account", [selectedOption]);
+  const isGroup = useMemo(() => selectedOption === "group", [selectedOption]);
+
+  const getOptions = async () => {
+    return DEFAULT_OPTIONS;
+  };
+
+  const handleGetOptions = async () => {
+    const filteredOptions = await getOptions();
+    setOptions(filteredOptions);
+  };
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+  };
+
+  const handleFormDataChange = (data: FormData) => {
+    setFormData(data);
+  };
+
+  const handleAccountSave = async () => {
+    if (!isFormValid) return;
+
+    if (isAccount) {
+      await updateAccount();
+    }
+
+    setIsModalOpen(true);
+  };
+
+  const updateAccount = async () => {
+    try {
+      // TODO: update account
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const validateForm = (): boolean => {
+    if (isAccount) {
+      const { email, firstName, lastName, password, number } = formData as AccountFormData;
+      return true;
+    }
+    return true;
+  };
+
+  const handleModal = (value: boolean) => {
+    setIsModalOpen(value);
+  };
+
+  useEffect(() => {
+    handleGetOptions();
+  }, []);
+
+  useEffect(() => {
+    if (selectedOption === "account") {
+      // TODO: fetchAccountDetails();
+    }
+  }, [selectedOption]);
+
+  useEffect(() => {
+    if (validateForm()) {
+      setIsFormValid(true);
+      return;
+    }
+    setIsFormValid(false);
+  }, [formData]);
+
+  return {
+    selectedOption,
+    options,
+    handleOptionChange,
+    handleModal,
+    formData,
+    handleFormDataChange,
+    handleAccountSave,
+    isFormValid,
+    isModalOpen,
+    isAccount
+  };
+};
