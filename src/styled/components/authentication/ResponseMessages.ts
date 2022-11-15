4e2e52b3dc6dc6f754813f249eaae77fa0a8b863
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-11-15 22:27:40 +0100

    Modified namings

--- src/styled/components/authentication/ResponseMessages.ts
+++ src/styled/components/authentication/ResponseMessages.ts
@@ -1,5 +1,5 @@
 export enum LoginResponseMessages {
-  Null = "",
+  Empty = "",
   Invalid = "Your provided email is invalid. Provide correct email or sign up.",
   Valid = "Your provided email is valid. Insert your password and sign in.",
   Error = "The error has occured when trying to sign in. Please try again."
