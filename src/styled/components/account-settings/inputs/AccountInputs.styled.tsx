f7c656355bff9f42bd29183200bfbe6d2abc118e
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-11-22 16:01:36 +0100

    Modified styles

--- src/styled/components/account-settings/inputs/AccountInputs.styled.tsx
+++ src/styled/components/account-settings/inputs/AccountInputs.styled.tsx
@@ -1,6 +1,7 @@
 import hexToRgba from "hex-to-rgba";
 import styled from "styled-components";
 import { Colors } from "styled/base/Colors";
+import { device } from "styled/base/Responsive";
 
 export const TableWrapper = styled.section`
   width: 100%;
@@ -15,14 +16,15 @@ export const GroupsColumnNames = styled.div`
   height: 1.8rem;
   padding: 0 1em;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   border-radius: 4px;
   background-color: ${hexToRgba(Colors.DarkGrayColor, 0.15)};
 `;
 
 export const GroupsColumnNamesItem = styled.span`
-  width: 30%;
+  width: 25%;
+  margin-right: 1em;
   font-size: 0.55em;
   font-family: InterRegular;
   letter-spacing: 0.1em;
@@ -36,7 +38,7 @@ export const Group = styled.div`
   height: fit-content;
   padding: 1.5em 1em;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   background-color: inherit;
   transition: background-color 0.2s ease-in-out;
@@ -47,7 +49,47 @@ export const Group = styled.div`
 `;
 
 export const GroupsItem = styled.div`
+  width: 25%;
+  margin-right: 1em;
   font-size: 0.8em;
   text-align: left;
-  width: 30%;
+`;
+
+export const Button = styled.button`
+  width: 20%;
+  margin: 0;
+  padding: 0.3rem 1.5rem;
+  text-align: center;
+  border: 1px solid ${Colors.RedColor};
+  border-radius: 4px;
+  background-color: ${hexToRgba(Colors.RedColor, 0.25)};
+  font-size: 0.7em;
+  color: ${Colors.RedColor};
+  transition: background-color 0.2s linear;
+
+  &:hover,
+  &:focus {
+    padding: 0.3rem 1.5rem;
+    font-size: 0.7em;
+    border: 1px solid ${Colors.RedColor};
+    background-color: ${hexToRgba(Colors.RedColor, 0.35)};
+    color: ${Colors.RedColor};
+  }
+
+  &:focus {
+    background-color: ${hexToRgba(Colors.RedColor, 0.3)};
+  }
+
+  &:disabled {
+    background-color: ${hexToRgba(Colors.RedColor, 0.15)};
+    border: 1px solid ${Colors.LightGrayColor};
+    color: ${Colors.LightGrayColor};
+    pointer-events: none;
+    user-select: none;
+  }
+
+  ${device.tablet} {
+    width: 100%;
+    margin-bottom: 2.5rem;
+  }
 `;
