1ac2053a6d3f4da94b64ef1b2749187f78f97c6b
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-12-17 14:41:50 +0100

    Imported options and imported the hook

--- src/styled/components/feed/feed-main/FeedMain.tsx
+++ src/styled/components/feed/feed-main/FeedMain.tsx
@@ -1,11 +1,15 @@
 import PageInformation from "styled/components/shared/page-info/PageInformation";
 import * as SS from "styled/components/shared/page-wrapper/PageWrapper.styled";
+import RowMenu from "styled/components/shared/row-menu/RowMenu";
+import { useFeed } from "../useFeed";
 
 const FeedMain = () => {
+  const { selectedOption, options, handleOptionChange, isLoading, events, handleRequest } = useFeed();
   return (
     <SS.ContentWrapper>
       <SS.ContentContainer>
         <PageInformation header={"Feed & Alerts"} paragraph={"Manage your requests. Accept or deny selected alerts."} />
+        <RowMenu selectedOption={selectedOption} handleOptionChange={handleOptionChange} options={options} />
       </SS.ContentContainer>
     </SS.ContentWrapper>
   );
