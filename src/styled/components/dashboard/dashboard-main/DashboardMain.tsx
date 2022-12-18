1b6192a42e80eadda80c9fb315f306def9fe4164
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-12-18 14:21:33 +0100

    Added loading state

--- src/styled/components/dashboard/dashboard-main/DashboardMain.tsx
+++ src/styled/components/dashboard/dashboard-main/DashboardMain.tsx
@@ -7,9 +7,12 @@ import { RootState } from "redux/store";
 import { useSelector } from "react-redux";
 import * as S from "./DashboardMain.styled";
 import * as SS from "styled/components/shared/page-wrapper/PageWrapper.styled";
+import { Wrapper } from "styled/elements/shared/wrappers/Wrapper";
+import { Loader } from "styled/elements/shared/Loader";
 
 const DashboardMain = () => {
   const currentDate = useSelector((state: RootState) => state.calendar.date);
+  const [isLoading, setIsLoading] = useState(true);
   const [events, setEvents] = useState<null | Event[]>();
   const [shouldShowAll, setShouldShowAll] = useState(false);
   const currentShowedEvents = useMemo(() => {
@@ -25,17 +28,24 @@ const DashboardMain = () => {
   }, [currentDate]);
 
   const fetchEvents = async () => {
+    setIsLoading(true);
     try {
       const data = await getEvents({ date: currentDate });
       setEvents(data.events);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <SS.ContentWrapper customMargin={!shouldShowButton}>
-      {currentShowedEvents && currentShowedEvents.length > 0 ? (
+      {isLoading ? (
+        <Wrapper width={"100%"} height={"10rem"} display={"flex"}>
+          <Loader />
+        </Wrapper>
+      ) : currentShowedEvents && currentShowedEvents.length > 0 ? (
         <SS.ContentContainer>
           <EventSection events={currentShowedEvents} />
           {shouldShowButton && (
