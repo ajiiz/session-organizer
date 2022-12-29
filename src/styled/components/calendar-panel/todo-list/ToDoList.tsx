bd48cde20f527f7f7b1593a530544d8989510f77
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2022-12-29 15:06:32 +0100

    Added logic and components

--- src/styled/components/calendar-panel/todo-list/ToDoList.tsx
+++ src/styled/components/calendar-panel/todo-list/ToDoList.tsx
@@ -1,13 +1,79 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { DailyToDo } from "@prisma/client";
+import { createTask } from "network/tasks/createTask";
+import { getTasks } from "network/tasks/getTasks";
+import CloseIcon from "assets/icons/navigation-panel/close-icon.svg";
+import { StyledImage } from "styled/elements/shared/StyledImage";
+import { deleteTask } from "network/tasks/deleteTask";
 import * as S from "./ToDoList.styled";
 
 const ToDoList = () => {
-  const [taskList, setTaskList] = useState<string[]>([]);
+  const [taskList, setTaskList] = useState<DailyToDo[]>([]);
+  const [currentTask, setCurrentTask] = useState<string>("");
+
+  const handleSubmit = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const task = (event.target as HTMLInputElement).value;
+      addTask(task);
+    }
+  };
+
+  const handleRemoveTask = async (taskId: string) => {
+    removeTask(taskId);
+  };
+
+  const addTask = async (task: string) => {
+    try {
+      await createTask({ task });
+      getTaskList();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setCurrentTask("");
+    }
+  };
+
+  const removeTask = async (taskId: string) => {
+    try {
+      await deleteTask({ taskId });
+      getTaskList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getTaskList = async () => {
+    try {
+      const data = await getTasks();
+      setTaskList(data.tasks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTaskList();
+  }, []);
 
   return (
     <S.ToDoListWrapper>
       <S.ToDoListHeader>Todays todo</S.ToDoListHeader>
-      <S.ToDoListInput type="text" placeholder="Add new task" />
+      <S.ToDoListInput
+        value={currentTask}
+        type="text"
+        placeholder="Add new task"
+        onKeyDown={handleSubmit}
+        onChange={event => setCurrentTask(event.target.value)}
+        maxLength={18}
+      />
+      <S.TaskList>
+        {taskList.map(task => (
+          <S.TaskItemWrapper>
+            <S.TaskItem key={task.id}>{task.text}</S.TaskItem>
+            <StyledImage src={CloseIcon} alt="Close icon" onClick={() => handleRemoveTask(task.id)} />
+          </S.TaskItemWrapper>
+        ))}
+      </S.TaskList>
     </S.ToDoListWrapper>
   );
 };
